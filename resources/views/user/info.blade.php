dfb8254073cae02a86c476949512a04cd406e62b
kikipermatasari <kikipermata0@gmail.com>
2020-09-21 15:45:31 +0700

    info lagi lagi

--- resources/views/user/info.blade.php
+++ resources/views/user/info.blade.php
@@ -226,10 +226,10 @@
                                  <p><a href="https://www.google.com/maps?saddr=My+Location&daddr=-0.4737957,100.423606,19" target="_blank"><button class="btn btn-sm btn-info">Cek Lokasi</button></a></p> 
 
                                 <p><a href="https://www.google.com/maps?saddr=My+Location&daddr=-0.4737957,100.423606,19" target="_blank"><button class="btn btn-sm btn-info">Cek Lokasi</button></a></p> 
-
+                                <br><br><br>
                                 <p><a href="https://www.google.com/maps?saddr=My+Location&daddr=-0.4737957,100.423606,19" target="_blank"><button class="btn btn-sm btn-info">Cek Lokasi</button></a></p>
 
-                                <br><br><br>
+                                
                                 <p><a href="https://www.google.com/maps?saddr=My+Location&daddr=-0.459911,100.418245" target="_blank"><button class="btn btn-sm btn-info">Cek Lokasi</button></a></p>
 
                                 <p><a href="https://www.google.com/maps?saddr=My+Location&daddr=-0.463149,100.399144" target="_blank"><button class="btn btn-sm btn-info">Cek Lokasi</button></a></p>
